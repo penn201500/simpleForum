64fc91f3e75ecedcc0a9f07075d94bd2ffb750e0
penn <penn201500@gmail.com>
2024-03-29 21:53:27 +0800

    use express.router to make files organized and code clean

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
+const router = require("./router");
+
 
 app.use(express.static("public"));
 app.set("views", "viewFiles");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home-guest");
-});
+app.use("/", router);
+app.use("/about", router);
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
