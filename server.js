385e21d264ed511823a6d854c646767771a25112
penn <penn201500@gmail.com>
2024-04-14 12:40:56 +0800

    store user into mongodb when registration

--- server.js
+++ server.js
@@ -2,7 +2,27 @@ const app = require("./app");
 const logger = require("./logger");
 const { ObjectId } = require("mongodb");
 const { connectToDB, getCollection, closeDBConnection } = require("./db");
+const listeningPort = 3001;
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
-});
+let dbCollection;
+
+async function run() {
+    try {
+        await connectToDB();
+        dbCollection = await getCollection();
+        app.listen(listeningPort, () => {
+            logger.info(`server is running on port ${listeningPort}`);
+        });
+    } catch (e) {
+        logger.error(e);
+        await closeDBConnection(); // Ensure closing the database connection on error
+        process.exit(1); // exit with a failure code
+    }
+}
+
+run().catch(async (e) => {
+        logger.error(e);
+        await closeDBConnection();
+        process.exit(1);
+    }
+);
