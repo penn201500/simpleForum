85a38056a1238a449afbc2a43834779a8446d9c4
penn <penn201500@gmail.com>
2024-04-14 12:12:08 +0800

    bugfix: remove 1 redundant if statement

--- models/User.js
+++ models/User.js
@@ -30,9 +30,6 @@ User.prototype.validate = function () {
     if (this.data.username === "") {
         this.errors.push("You must provide a username.");
     }
-    if (this.data.email === "") {
-        this.errors.push("You must provide a valid email address.");
-    }
     if (this.data.password === "") {
         this.errors.push("You must provide a password.");
     }
