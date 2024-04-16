ef54ef258fed282a4ba49ec3711d912d2d989794
penn <penn201500@gmail.com>
2024-04-16 22:54:11 +0800

    use session to remember user from one http request to another

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const router = require("./router");
 
+let sessionOptions = session({
+    secret: "JavaScript is so cool", // a secret key used to sign the session ID cookie
+    resave: false, // forces the session to be saved back to the session store
+    saveUninitialized: false, // forces a session that is "uninitialized" to be saved to the store
+    cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }, // 24 hours
+})
+
 // middleware
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(sessionOptions);
 
 app.use(express.static("public"));
 app.set("views", "viewFiles");
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,24 @@
 const User = require("../models/User");
 
 const home = (req, res) => {
-  res.render("home-guest");
+  if (req.session.user) {
+    // res.render("home-dashboard", { username: req.session.user.username });
+    res.send("Welcome back, " + req.session.user.username);
+  } else {
+    res.render("home-guest");
+  }
 };
 
 function login(req, res) {
+  // http requests are stateless, we use sessions to store user data
+  // session is a way to store data on the server temporarily
+  // our server will remember the user's data until the session expires
+  // so we can use this session data from any of our URLs or routes
   let user = new User(req.body);
   user
     .login()
     .then((result) => {
+      req.session.user = { username: user.data.username }; // to make the session object unique to per user
       res.send(result);
     })
     .catch((e) => {
