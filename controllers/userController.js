819bd9593539d32053c0fdb1eaa31bb768a9e404
penn <penn201500@gmail.com>
2024-04-17 00:58:33 +0800

    render home-dashboard page after login

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,7 @@ const User = require("../models/User");
 
 const home = (req, res) => {
   if (req.session.user) {
-    // res.render("home-dashboard", { username: req.session.user.username });
-    res.send("Welcome back, " + req.session.user.username);
+    res.render("home-dashboard", { username: req.session.user.username });
   } else {
     res.render("home-guest");
   }
