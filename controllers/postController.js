42cf542270f458ab82c58d2c533e4535c49868b3
penn <penn201500@gmail.com>
2024-04-21 16:40:54 +0800

    add create-post page and functions

---
+++ controllers/postController.js
@@ -0,0 +1,3 @@
+exports.createPost = function (req, res) {
+    res.render("create-post");
+}
--- router.js
+++ router.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("./controllers/userController");
+const postController = require('./controllers/postController');
 
+// user related routes
 router.get("/", userController.home);
 router.post("/registration", userController.registration);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
+
+// post related routes
+router.get("/create-post", postController.createPost);
 
 router.get("/about", (req, res) => {
     res.send("This is an about page");
