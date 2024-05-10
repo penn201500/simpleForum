4260402f0ccd65a9e6640d9621282d7475952109
penn <penn201500@gmail.com>
2024-05-10 23:45:57 +0800

    add get create-post page

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,19 @@
+const Post = require("../models/Post");
+
+exports.viewCreateScreen = function (req, res) {
+  res.render("create-post");
+};
+
 exports.createPost = function (req, res) {
-    res.render("create-post");
-}
+  let post = new Post(req.body, req.session.user._id);
+  post
+    .create()
+    .then(function (newId) {
+      req.flash("success", "New post successfully created.");
+      req.session.save(() => res.redirect(`/post/${newId}`));
+    })
+    .catch(function (errors) {
+      post.errors.forEach((error) => req.flash("errors", error));
+      req.session.save(() => res.redirect("/create-post"));
+    });
+};
--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("./controllers/userController");
-const postController = require('./controllers/postController');
+const postController = require("./controllers/postController");
 
 // user related routes
 router.get("/", userController.home);
@@ -9,10 +9,18 @@ router.post("/registration", userController.registration);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 
-// post related routes
-router.get("/create-post", userController.mustBeLoggedIn, postController.createPost);
+router.get(
+  "/create-post",
+  userController.mustBeLoggedIn,
+  postController.viewCreateScreen
+);
+router.post(
+  "/create-post",
+  userController.mustBeLoggedIn,
+  postController.createPost
+);
 
 router.get("/about", (req, res) => {
-    res.send("This is an about page");
+  res.send("This is an about page");
 });
 module.exports = router;
