d194df6461ae0ce3101b1d1c137f907e28e391a1
penn <penn201500@gmail.com>
2024-05-20 13:33:25 +0800

    for security reason, user must logged in to see edit page or do edit action

--- router.js
+++ router.js
@@ -10,26 +10,14 @@ router.post("/login", userController.login);
 router.post("/logout", userController.logout);
 
 // profile related routes
-router.get(
-  "/profile/:username",
-  userController.ifUserExists,
-  userController.profilePostsScreen
-);
+router.get("/profile/:username", userController.ifUserExists, userController.profilePostsScreen);
 
 // post related routes
-router.get(
-  "/create-post",
-  userController.mustBeLoggedIn,
-  postController.viewCreateScreen
-);
-router.post(
-  "/create-post",
-  userController.mustBeLoggedIn,
-  postController.createPost
-);
+router.get("/create-post", userController.mustBeLoggedIn, postController.viewCreateScreen);
+router.post("/create-post", userController.mustBeLoggedIn, postController.createPost);
 router.get("/post/:id", postController.viewSingle);
-router.get("/post/:id/edit", postController.viewEditScreen);
-router.post("/post/:id/edit", postController.edit);
+router.get("/post/:id/edit", userController.mustBeLoggedIn, postController.viewEditScreen);
+router.post("/post/:id/edit", userController.mustBeLoggedIn, postController.edit);
 
 router.get("/about", (req, res) => {
   res.send("This is an about page");
