8fa4c65a7fdffe634759f0594f60deba1b4567f6
penn <penn201500@gmail.com>
2024-05-30 21:22:07 +0800

    make posts page title tag dynamic

--- controllers/postController.js
+++ controllers/postController.js
@@ -25,7 +25,7 @@ exports.createPost = function (req, res) {
 exports.viewSingle = async function (req, res) {
   try {
     let post = await Post.findSingleById(req.params.id, req.visitorId);
-    res.render("single-post-screen", { post: post });
+    res.render("single-post-screen", { post: post, title: post.title });
   } catch (error) {
     res.render("404");
   }
