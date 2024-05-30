06cdc51e8ec5ab0bcfd0ae692874be3a235a321c
penn <penn201500@gmail.com>
2024-05-30 13:09:45 +0800

    add feature: emit chat message to other users

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const session = require("express-session");
-const mongoStore = require("connect-mongo");
-const router = require("./router");
-const { client, dbName } = require("./db");
-const flash = require("connect-flash");
-const markdown = require("marked");
-const app = express();
-const sanitizeHtml = require("sanitize-html");
+const express = require("express")
+const session = require("express-session")
+const mongoStore = require("connect-mongo")
+const router = require("./router")
+const { client, dbName } = require("./db")
+const flash = require("connect-flash")
+const markdown = require("marked")
+const app = express()
+const sanitizeHtml = require("sanitize-html")
 
 let sessionOptions = session({
   secret: "JavaScript is so cool", // a secret key used to sign the session ID cookie
@@ -14,39 +14,49 @@ let sessionOptions = session({
   resave: false, // forces the session to be saved back to the session store
   saveUninitialized: false, // forces a session that is "uninitialized" to be saved to the store
   cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }, // 24 hours
-});
+})
 
 // middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(sessionOptions);
-app.use(flash());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(sessionOptions)
+app.use(flash())
 app.use((req, res, next) => {
   // make our markdown function available from within ejs templates
   res.locals.filterUserHTML = function (content) {
-    return sanitizeHtml(markdown.parse(content), { allowedTags: ['p', 'br', 'ul', 'ol', 'li', 'strong', 'bold', 'i', 'em', 'h1', 'h2', 'h3', 'h4' ], allowedAttributes: {} });
-  };
+    return sanitizeHtml(markdown.parse(content), { allowedTags: ["p", "br", "ul", "ol", "li", "strong", "bold", "i", "em", "h1", "h2", "h3", "h4"], allowedAttributes: {} })
+  }
   // make all error and success flash messages available from all templates
-  res.locals.errors = req.flash("errors");
-  res.locals.success = req.flash("success");
+  res.locals.errors = req.flash("errors")
+  res.locals.success = req.flash("success")
   // make current user id available on the req object
   if (req.session.user) {
-    req.visitorId = req.session.user._id;
+    req.visitorId = req.session.user._id
   } else {
-    req.visitorId = 0;
+    req.visitorId = 0
   }
   // make user session data available from within view templates
-  res.locals.user = req.session.user;
-  next();
-});
+  res.locals.user = req.session.user
+  next()
+})
 
-app.use(express.static("public"));
-app.set("views", "viewFiles");
-app.set("view engine", "ejs");
+app.use(express.static("public"))
+app.set("views", "viewFiles")
+app.set("view engine", "ejs")
 
-app.use("/", router);
-app.use("/login", router);
-app.use("/registration", router);
-app.use("/about", router);
+app.use("/", router)
+app.use("/login", router)
+app.use("/registration", router)
+app.use("/about", router)
 
-module.exports = app;
+const server = require("http").createServer(app)
+
+const io = require("socket.io")(server)
+
+io.on("connection", function (socket) {
+  socket.on("messageFromBrowser", function (data) {
+    io.emit("messageToBrowser", {message: data.message})
+  })
+})
+
+module.exports = server
--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -1,33 +1,59 @@
 export default class Chat {
   constructor() {
-    this.chatWrapper = document.querySelector("#chat-wrapper");
-    this.openIcon = document.querySelector(".header-chat-icon");
-    this.injectHTML();
-    this.closeIcon = document.querySelector(".chat-title-bar-close");
-    this.events();
+    this.openedYet = false
+    this.chatWrapper = document.querySelector("#chat-wrapper")
+    this.openIcon = document.querySelector(".header-chat-icon")
+    this.injectHTML()
+    this.chatField = document.querySelector("#chatField")
+    this.chatForm = document.querySelector("#chatForm")
+    this.closeIcon = document.querySelector(".chat-title-bar-close")
+    this.events()
   }
 
   // Events
   events() {
     this.openIcon.addEventListener("click", e => {
-      e.preventDefault();
-      this.showChat();
-    });
+      e.preventDefault()
+      this.showChat()
+    })
     this.closeIcon.addEventListener("click", e => {
-      e.preventDefault();
-      this.closeChat();
+      e.preventDefault()
+      this.closeChat()
+    })
+    this.chatForm.addEventListener("submit", e => {
+      e.preventDefault()
+      this.sendMessageToServer()
     })
   }
 
   showChat() {
-    this.chatWrapper.classList.add("chat--visible");
+    if (!this.openedYet) {
+      // open a new connection
+      this.openConnection()
+    }
+    this.openedYet = true
+    this.chatWrapper.classList.add("chat--visible")
+  }
+
+  openConnection() {
+    // Open a new connection
+    this.socket = io()
+    this.socket.on("messageToBrowser", function (data) {
+      alert(data.message)
+    })
   }
 
   closeChat() {
-    this.chatWrapper.classList.remove("chat--visible");
+    this.chatWrapper.classList.remove("chat--visible")
   }
 
   // Methods
+  sendMessageToServer() {
+    this.socket.emit("messageFromBrowser", { message: this.chatField.value })
+    this.chatField.value = ""
+    this.chatField.focus()
+  }
+
   injectHTML() {
     this.chatWrapper.innerHTML = `
     <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
@@ -36,6 +62,6 @@ export default class Chat {
     <form id="chatForm" class="chat-form border-top">
     <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
     </form>
-    `;
+    `
   }
 }
